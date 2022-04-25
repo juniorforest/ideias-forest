928262a50a3b1311d157ed0d281b6bd15d978363
unknown <xforest91@gmail.com>
2022-04-25 11:23:37 -0300

    ideias-forest

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,5 @@
-
-
-
 function Home() {
-    return <div>
-       <h1>Que hojé sejá um dia abençoado</h1>
-       <p>
-           Vou me mostrar presente pra sempre estar junto a você meu amor 
-       </p>
-
-
-
-        </div>
-      
-
-        
-        
+    return <div>Home</div>      
 }
 
 
