432e6bd9f21904301607ec7cf62ac3f44728ca99
unknown <xforest91@gmail.com>
2022-04-25 11:10:41 -0300

    ideias-forest

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,12 @@
-import { useState } from 'react';
+
 
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Contador />
-            <div>rebeca</div>
-        </div>
-    )    
+    return <div>Home</div>
+        
+        
 }
 
-function Contador() {
-    const [contador,setContador] = useState(1);
 
-    function adicionarContador() {
-        setContador(contador + 1);
-    }    
-        return(
-            <div>
-                <div>{contador}</div>
-                <button onClick={adicionarContador}>adicionar</button>
-            </div>
-        )    
-    
-}
 
 export default Home
